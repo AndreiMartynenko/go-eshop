7cdd1ca52de3665882b5fcba6c49ab02768bd018
AndrewLondon <andrewmart@mail.com>
2024-01-07 06:43:21 +0000

    rest server implemented

--- cmd/server/rest_server.go
+++ cmd/server/rest_server.go
@@ -7,3 +7,14 @@ type RestServer struct {
 	server       *http.Server
 	orderService OrderServiceServer // The same order service as in the gRPC server
 }
+
+
+// The NewRestServer function is perfect for creating a RestServer
+func NewRestServer(orderService OrderServiceServer, port string) RestServer {
+	rs := RestServer{
+		server: &http.Server{
+			Addr:    ":" + port,
+			Handler: router,
+		},
+		orderService: orderService,
+	}
