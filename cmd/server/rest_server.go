94f570a348e20d7f5378abd2338de376e76bc05e
AndrewLondon <andrewmart@mail.com>
2024-01-07 06:42:58 +0000

    rest server implemented

--- cmd/server/rest_server.go
+++ cmd/server/rest_server.go
@@ -1,1 +1,9 @@
+package main
 
+import "net/http"
+
+// RestServer implements a REST server for the order service
+type RestServer struct {
+	server       *http.Server
+	orderService OrderServiceServer // The same order service as in the gRPC server
+}
